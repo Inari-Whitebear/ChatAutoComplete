dfeabf60799c374f91f145b53a0e46adeb142ad1
Neptune Whitebear <manatsu.yuhara@gmail.com>
2011-10-01 21:48:06 +0200

    changed formatting, fixed regex

--- plugin/whitebear/neptune/ChatAutoComplete/ChatAutoComplete.java
+++ plugin/whitebear/neptune/ChatAutoComplete/ChatAutoComplete.java
@@ -10,85 +10,94 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.config.Configuration;
 
-
 public class ChatAutoComplete extends JavaPlugin
 {
 
-	@Override
-	public void onDisable() {
-		consoleMsg( "Disabled." );
+    @Override
+    public void onDisable()
+    {
+        consoleMsg( "Disabled." );
 
-	}
+    }
 
-	@Override
-	public void onEnable() {
+    @Override
+    public void onEnable()
+    {
 
-		PluginDescriptionFile pluginDesc = getDescription();
-		prefix = new StringBuilder( "[" ).append( pluginDesc.getName() )
-				.append( " (" ).append( pluginDesc.getVersion() )
-				.append( ")] " ).toString();
-		mcLogger = Logger.getLogger( "Minecraft" );
-		readConfig();
+        PluginDescriptionFile pluginDesc = getDescription();
+        prefix = new StringBuilder( "[" ).append( pluginDesc.getName() )
+                .append( " (" ).append( pluginDesc.getVersion() )
+                .append( ")] " ).toString();
+        mcLogger = Logger.getLogger( "Minecraft" );
+        readConfig();
 
-		if ( maxReplace <= 0 ) {
-			consoleMsg( "Can't replace // Max Relace Amount is set to 0 or lower." );
-			super.getPluginLoader().disablePlugin( this );
-			return;
-		}
+        if ( maxReplace <= 0 )
+        {
+            consoleMsg( "Can't replace // Max Relace Amount is set to 0 or lower." );
+            super.getPluginLoader().disablePlugin( this );
+            return;
+        }
 
-		playerListener = new ChatAutoCompletePlayerListener( this, chatPrefix,
-				maxReplace, colorAt );
+        playerListener = new ChatAutoCompletePlayerListener( this, chatPrefix,
+                maxReplace, colorAt );
 
-		PluginManager pgnMng = this.getServer().getPluginManager();
+        PluginManager pgnMng = this.getServer().getPluginManager();
 
-		pgnMng.registerEvent( Type.PLAYER_CHAT, playerListener,
-				Priority.Normal, this );
+        pgnMng.registerEvent( Type.PLAYER_CHAT, playerListener,
+                Priority.Normal, this );
 
-		consoleMsg( "Enabled." );
-	}
+        consoleMsg( "Enabled." );
+    }
 
-	private void readConfig() {
-		Configuration config = this.getConfiguration();
-		config.load();
-		setDefaults( config );
-		config.save();
+    private void readConfig()
+    {
+        Configuration config = this.getConfiguration();
+        config.load();
+        setDefaults( config );
+        config.save();
 
-		chatPrefix = config.getString( "chatPrefix" ).trim().charAt( 0 );
-		maxReplace = config.getInt( "maxReplace", 10 );
-		colorAt = config.getString( "color" );
+        chatPrefix = config.getString( "chatPrefix" ).trim().charAt( 0 );
+        maxReplace = config.getInt( "maxReplace", 10 );
+        colorAt = config.getString( "color" );
 
-	}
+    }
 
-	private void setDefaults( Configuration config ) {
-		Map<String,Object> nodeMap = config.getAll();
-		config.setHeader( "#ChatAutoComplete Config","#chatPrefix = prefix to use before names so they get auto-completed","#maxReplace = maximum unique names replaced in a single chat message", "#color = Color to put names in; use '-1' (in quotes) to disable." );
-		if ( !nodeMap.containsKey( "chatPrefix" ) )
-		{
-			config.setProperty( "chatPrefix", "@" );
-			
-		}
-		if ( !nodeMap.containsKey( "maxReplace" ) )
-			config.setProperty( "maxReplace", 10 );
-		if ( !nodeMap.containsKey( "color" ) )
-			config.setProperty( "color", "4" );
-	}
+    private void setDefaults( Configuration config )
+    {
+        Map<String, Object> nodeMap = config.getAll();
+        config.setHeader(
+                "#ChatAutoComplete Config",
+                "#chatPrefix = prefix to use before names so they get auto-completed",
+                "#maxReplace = maximum unique names replaced in a single chat message",
+                "#color = Color to put names in; use '-1' (in quotes) to disable." );
+        if ( ! nodeMap.containsKey( "chatPrefix" ) )
+        {
+            config.setProperty( "chatPrefix", "@" );
 
-	public void consoleMsg( String msg ) {
-		mcLogger.info( prefix + msg );
+        }
+        if ( ! nodeMap.containsKey( "maxReplace" ) )
+            config.setProperty( "maxReplace", 10 );
+        if ( ! nodeMap.containsKey( "color" ) )
+            config.setProperty( "color", "4" );
+    }
 
-	}
+    public void consoleMsg( String msg )
+    {
+        mcLogger.info( prefix + msg );
 
-	public ChatAutoComplete()
-	{
+    }
 
-	}
+    public ChatAutoComplete()
+    {
 
-	ChatAutoCompletePlayerListener	playerListener;
-	String							prefix	= "";
-	Logger							mcLogger;
-	int								maxReplace;
-	int								chatPrefix;
+    }
 
-	String							colorAt;
+    ChatAutoCompletePlayerListener playerListener;
+    String                         prefix = "";
+    Logger                         mcLogger;
+    int                            maxReplace;
+    int                            chatPrefix;
+
+    String                         colorAt;
 
 }
--- plugin/whitebear/neptune/ChatAutoComplete/ChatAutoCompletePlayerListener.java
+++ plugin/whitebear/neptune/ChatAutoComplete/ChatAutoCompletePlayerListener.java
@@ -10,62 +10,73 @@ import org.bukkit.event.player.PlayerListener;
 public class ChatAutoCompletePlayerListener extends PlayerListener
 {
 
-	public ChatAutoCompletePlayerListener( ChatAutoComplete cPlugin,
-			int cCharPrefix, int cMaxReplace, String cColor )
-	{
-		plugin = cPlugin;
-		charPrefix = cCharPrefix;
-		maxReplace = cMaxReplace;
-		color = ChatColor.getByCode( Integer.parseInt( cColor, 16 ) );
+    public ChatAutoCompletePlayerListener( ChatAutoComplete cPlugin,
+            int cCharPrefix, int cMaxReplace, String cColor )
+    {
+        plugin = cPlugin;
+        charPrefix = cCharPrefix;
+        maxReplace = cMaxReplace;
+        // Convert color code to ChatColor
+        color = ChatColor.getByCode( Integer.parseInt( cColor, 16 ) );
 
-	}
+    }
 
-	public void onPlayerChat( PlayerChatEvent event ) {
-		if ( event.isCancelled() ) return;
-		Player sender = event.getPlayer();
-		if ( ! sender.hasPermission( "autocomp.autocomp" ) ) return;
+    public void onPlayerChat( PlayerChatEvent event )
+    {
+        if ( event.isCancelled() ) return;
+        Player sender = event.getPlayer();
+        if ( ! sender.hasPermission( "autocomp.autocomp" ) ) return;
+        // Escape if cancelled or doesn't have permissions
 
-		String msg = event.getMessage();
-		int lastIndex = 0;
-		int position = msg.indexOf( charPrefix, lastIndex );
-		int safeLoop = maxReplace;
+        String msg = event.getMessage();
+        int lastIndex = 0;
+        int position = msg.indexOf( charPrefix, lastIndex );
+        int safeLoop = maxReplace;
 
-		while ( position != - 1 && safeLoop > 0 ) {
-			lastIndex = position + 2;
-			if ( lastIndex > msg.length() ) break;
+        // Loop until nothing else to replace or maximum replaces reached
+        while ( position != - 1 && safeLoop > 0 )
+        {
 
-			int nextSpace = msg.indexOf( ' ', position );
-			String subName = msg.substring( position + 1,
-					(nextSpace == - 1 ? msg.length() : nextSpace) );
-			if ( subName.length() != 0 ) {
-				Player player = plugin.getServer().getPlayer( subName );
-				if ( player != null ) {
-					msg = msg.replaceAll(
-							new StringBuilder( "[^a-zA-Z0-9_\\-" )
-									.append( charPrefix ).append( "]" )
-									.append( Pattern.quote( (subName) ) )
-									.append( "\\b" ).toString(),
-							new StringBuilder( ( (color == null ? "" : color
-									.toString())) )
-									.append( ((char) charPrefix) )
-									.append( player.getName() )
-									.append( ChatColor.WHITE.toString() )
-									.toString() );
+            // Find next space after the @ and extract the name
+            int nextSpace = msg.indexOf( ' ', position );
+            String subName = msg.substring( position + 1,
+                    (nextSpace == - 1 ? msg.length() : nextSpace) );
 
-					event.setMessage( msg );
-					safeLoop -- ;
-				} else {
+            if ( subName.length() != 0 )
+            {
 
-				}
-				position = msg.indexOf( charPrefix, lastIndex );
-			}
-		}
+                Player player = plugin.getServer().getPlayer( subName );
+                if ( player != null )
+                {
+                    // Replace all occurences with the complete name and color
+                    msg = msg.replaceAll(
+                            new StringBuilder( "(^|\\s)" )                                    
+                                    .append( Pattern.quote( (char)charPrefix +(subName) ) )
+                                    .append( "($|\\s)" ).toString(),
+                            new StringBuilder( "$1") .append( ( (color == null ? "" : color
+                                    .toString())) )
+                                    .append( ((char) charPrefix) )
+                                    .append( player.getName() )
+                                    .append( ChatColor.WHITE.toString() ).append("$2")
+                                    .toString() );
 
-	}
+                    event.setMessage( msg );
+                    safeLoop -- ;
+                }
 
-	ChatAutoComplete	plugin;
-	int					charPrefix;
-	int					maxReplace;
-	ChatColor			color;
+            }
+            // Skip @ and first letter so the same one isn't found again
+            lastIndex = position + 2;
+            // Exit if end of string
+            if ( lastIndex > msg.length() ) break;
+            position = msg.indexOf( charPrefix, lastIndex );
+        }
+
+    }
+
+    ChatAutoComplete plugin;
+    int              charPrefix;
+    int              maxReplace;
+    ChatColor        color;
 
 }
