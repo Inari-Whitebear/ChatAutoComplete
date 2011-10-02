168ed6d2e663e13c9a52959ce02ab75fe9c53aa8
Neptune Whitebear <manatsu.yuhara@gmail.com>
2011-10-02 02:31:14 +0200

    v0.6 Beta - Added Essnetials Support - changed color functionality

--- plugin/whitebear/neptune/ChatAutoComplete/ChatAutoComplete.java
+++ plugin/whitebear/neptune/ChatAutoComplete/ChatAutoComplete.java
@@ -1,21 +1,21 @@
 /*
-*   This file is part of ChatAutoComplete.
-*   
-*   
-*   ChatAutoComplete is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   ChatAutoComplete is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with ChatAutoComplete. If not, see <http://www.gnu.org/licenses/>.
-*   
-*/
+ *   This file is part of ChatAutoComplete.
+ *   
+ *   
+ *   ChatAutoComplete is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ChatAutoComplete is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with ChatAutoComplete. If not, see <http://www.gnu.org/licenses/>.
+ *   
+ */
 
 package plugin.whitebear.neptune.ChatAutoComplete;
 
@@ -24,10 +24,14 @@ import java.util.logging.Logger;
 
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.config.Configuration;
+import org.anjocaido.groupmanager.permissions.NijikoPermissionsProxy;
+
+import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class ChatAutoComplete extends JavaPlugin
 {
@@ -44,26 +48,32 @@ public class ChatAutoComplete extends JavaPlugin
     {
 
         PluginDescriptionFile pluginDesc = getDescription();
-        prefix = new StringBuilder( "[" ).append( pluginDesc.getName() )
-                .append( " (" ).append( pluginDesc.getVersion() )
-                .append( ")] " ).toString();
+        prefix = "[" + pluginDesc.getName() + " (" + pluginDesc.getVersion() + ")] ";
         mcLogger = Logger.getLogger( "Minecraft" );
         readConfig();
 
-        if ( maxReplace <= 0 )
+        if( maxReplace <= 0 )
         {
             consoleMsg( "Can't replace // Max Relace Amount is set to 0 or lower." );
             super.getPluginLoader().disablePlugin( this );
             return;
         }
 
-        playerListener = new ChatAutoCompletePlayerListener( this, chatPrefix,
-                maxReplace, colorAt );
-
         PluginManager pgnMng = this.getServer().getPluginManager();
 
-        pgnMng.registerEvent( Type.PLAYER_CHAT, playerListener,
-                Priority.Normal, this );
+        if( useEssentials )
+        {
+            Plugin essentialsBridge = pgnMng.getPlugin( "Permissions" );
+            if( essentialsBridge != null )
+            {
+                essentialsProxy = (NijikoPermissionsProxy) ((Permissions) essentialsBridge).getHandler();
+                consoleMsg( "Using essentials" );
+            }
+        }
+
+        playerListener = new ChatAutoCompletePlayerListener( this, chatPrefix, maxReplace, atSignColor, essentialsProxy );
+
+        pgnMng.registerEvent( Type.PLAYER_CHAT, playerListener, Priority.Normal, this );
 
         consoleMsg( "Enabled." );
     }
@@ -77,27 +87,24 @@ public class ChatAutoComplete extends JavaPlugin
 
         chatPrefix = config.getString( "chatPrefix" ).trim().charAt( 0 );
         maxReplace = config.getInt( "maxReplace", 10 );
-        colorAt = config.getString( "color" );
+        atSignColor = config.getString( "atSignColor" );
+        useEssentials = config.getBoolean( "useEssentials", false );
 
     }
 
     private void setDefaults( Configuration config )
     {
         Map<String, Object> nodeMap = config.getAll();
-        config.setHeader(
-                "#ChatAutoComplete Config",
+        config.setHeader( "#ChatAutoComplete Config",
                 "#chatPrefix = prefix to use before names so they get auto-completed",
                 "#maxReplace = maximum unique names replaced in a single chat message",
-                "#color = Color to put names in; use '-1' (in quotes) to disable." );
-        if ( ! nodeMap.containsKey( "chatPrefix" ) )
-        {
-            config.setProperty( "chatPrefix", "@" );
+                "#atSignColor = Color used for the @ sign; use '-1' (in quotes) to disable."
+                        + "#useEssentials = using essentials for name prefixing" );
 
-        }
-        if ( ! nodeMap.containsKey( "maxReplace" ) )
-            config.setProperty( "maxReplace", 10 );
-        if ( ! nodeMap.containsKey( "color" ) )
-            config.setProperty( "color", "4" );
+        if( !nodeMap.containsKey( "chatPrefix" ) ) config.setProperty( "chatPrefix", "@" );
+        if( !nodeMap.containsKey( "maxReplace" ) ) config.setProperty( "maxReplace", 10 );
+        if( !nodeMap.containsKey( "atSignColor" ) ) config.setProperty( "atSignColor", "4" );
+        if( !nodeMap.containsKey( "useEssentials" ) ) config.setProperty( "useEssentials", false );
     }
 
     public void consoleMsg( String msg )
@@ -112,11 +119,12 @@ public class ChatAutoComplete extends JavaPlugin
     }
 
     ChatAutoCompletePlayerListener playerListener;
-    String                         prefix = "";
+    String                         prefix          = "";
     Logger                         mcLogger;
     int                            maxReplace;
     int                            chatPrefix;
-
-    String                         colorAt;
+    String                         atSignColor;
+    NijikoPermissionsProxy         essentialsProxy = null;
+    boolean                        useEssentials;
 
 }
--- plugin/whitebear/neptune/ChatAutoComplete/ChatAutoCompletePlayerListener.java
+++ plugin/whitebear/neptune/ChatAutoComplete/ChatAutoCompletePlayerListener.java
@@ -1,26 +1,27 @@
 /*
-*   This file is part of ChatAutoComplete.
-*   
-*   
-*   ChatAutoComplete is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   ChatAutoComplete is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with ChatAutoComplete. If not, see <http://www.gnu.org/licenses/>.
-*   
-*/
+ *   This file is part of ChatAutoComplete.
+ *   
+ *   
+ *   ChatAutoComplete is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ChatAutoComplete is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with ChatAutoComplete. If not, see <http://www.gnu.org/licenses/>.
+ *   
+ */
 
 package plugin.whitebear.neptune.ChatAutoComplete;
 
 import java.util.regex.Pattern;
 
+import org.anjocaido.groupmanager.permissions.NijikoPermissionsProxy;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerChatEvent;
@@ -29,73 +30,71 @@ import org.bukkit.event.player.PlayerListener;
 public class ChatAutoCompletePlayerListener extends PlayerListener
 {
 
-    public ChatAutoCompletePlayerListener( ChatAutoComplete cPlugin,
-            int cCharPrefix, int cMaxReplace, String cColor )
+    public ChatAutoCompletePlayerListener( ChatAutoComplete cPlugin, int cCharPrefix, int cMaxReplace, String cColor,
+            NijikoPermissionsProxy cEssentialsProxy )
     {
         plugin = cPlugin;
         charPrefix = cCharPrefix;
         maxReplace = cMaxReplace;
         // Convert color code to ChatColor
-        color = ChatColor.getByCode( Integer.parseInt( cColor, 16 ) );
-
+        atSignColor = ChatColor.getByCode( Integer.parseInt( cColor, 16 ) );
+        essentialsProxy = cEssentialsProxy;
     }
 
     public void onPlayerChat( PlayerChatEvent event )
     {
-        if ( event.isCancelled() ) return;
+        if( event.isCancelled() ) return;
         Player sender = event.getPlayer();
-        if ( ! sender.hasPermission( "autocomp.autocomp" ) ) return;
+        if( !sender.hasPermission( "autocomp.autocomp" ) ) return;
         // Escape if cancelled or doesn't have permissions
 
         String msg = event.getMessage();
         int lastIndex = 0;
         int position = msg.indexOf( charPrefix, lastIndex );
         int safeLoop = maxReplace;
-
         // Loop until nothing else to replace or maximum replaces reached
-        while ( position != - 1 && safeLoop > 0 )
+        while ( position != -1 && safeLoop > 0 )
         {
 
             // Find next space after the @ and extract the name
             int nextSpace = msg.indexOf( ' ', position );
-            String subName = msg.substring( position + 1,
-                    (nextSpace == - 1 ? msg.length() : nextSpace) );
+            String subName = msg.substring( position + 1, (nextSpace == -1 ? msg.length() : nextSpace) );
 
-            if ( subName.length() != 0 )
+            if( subName.length() != 0 )
             {
 
                 Player player = plugin.getServer().getPlayer( subName );
-                if ( player != null )
+
+                if( player != null )
                 {
+                    String prefix = ChatColor.WHITE.toString();
+                    if( essentialsProxy != null )
+                    {
+                        prefix = essentialsProxy.getUserPrefix( player.getWorld().getName(), player.getName() );
+                    }
                     // Replace all occurences with the complete name and color
-                    msg = msg.replaceAll(
-                            new StringBuilder( "(^|\\s)" )                                    
-                                    .append( Pattern.quote( (char)charPrefix +(subName) ) )
-                                    .append( "($|\\s)" ).toString(),
-                            new StringBuilder( "$1") .append( ( (color == null ? "" : color
-                                    .toString())) )
-                                    .append( ((char) charPrefix) )
-                                    .append( player.getName() )
-                                    .append( ChatColor.WHITE.toString() ).append("$2")
-                                    .toString() );
+                    msg = msg.replaceAll( "(^|\\s)" + Pattern.quote( (char) charPrefix + (subName) ) + "($|\\s)", "$1"
+                            + (atSignColor == null ? "" : atSignColor.toString()) + ((char) charPrefix) + prefix
+                            + player.getName() + ChatColor.WHITE.toString() + "$2" );
 
                     event.setMessage( msg );
-                    safeLoop -- ;
+                    safeLoop--;
                 }
 
             }
             // Skip @ and first letter so the same one isn't found again
             lastIndex = position + 2;
             // Exit if end of string
-            if ( lastIndex > msg.length() ) break;
+            if( lastIndex > msg.length() ) break;
             position = msg.indexOf( charPrefix, lastIndex );
         }
 
     }
 
-    ChatAutoComplete plugin;
-    int              charPrefix;
-    int              maxReplace;
-    ChatColor        color;
+    ChatAutoComplete       plugin;
+    int                    charPrefix;
+    int                    maxReplace;
+    ChatColor              atSignColor;
+    NijikoPermissionsProxy essentialsProxy;
 
 }
